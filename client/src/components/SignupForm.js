040155c866189730a8f7a5b2f1f0c615911651d2
Charlotte Wong <ChiharuTaru@gmail.com>
2021-07-19 09:53:16 -0700

    renders

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,53 +1,58 @@
-import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
-import Auth from '../utils/auth';
+import React, { useEffect, useState } from 'react'
+import { Form, Button, Alert } from 'react-bootstrap'
+import { useMutation } from '@apollo/client'
+import { ADD_USER } from '../utils/mutations'
+import Auth from '../utils/auth'
 
 const SignupForm = () => {
   // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' })
   // set state for form validation
-  const [validated] = useState(false);
+  const [validated] = useState(false)
   // set state for alert
-  const [showAlert, setShowAlert] = useState(false);
+  const [showAlert, setShowAlert] = useState(false)
+
+  const [addUser, { error }] = useMutation(ADD_USER)
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true)
+    } else {
+      setShowAlert(false)
+    }
+  }, [error])
 
   const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
-  };
+    const { name, value } = event.target
+    setUserFormData({ ...userFormData, [name]: value })
+  }
 
   const handleFormSubmit = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
+    const form = event.currentTarget
     if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
+      event.preventDefault()
+      event.stopPropagation()
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await addUser({
+        variables: { ...userFormData }
+      })
+      Auth.login(data.addUser.token)
     } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+      console.error(err)
+      setShowAlert(true)
     }
 
     setUserFormData({
       username: '',
       email: '',
-      password: '',
-    });
-  };
+      password: ''
+    })
+  }
 
   return (
     <>
@@ -99,12 +104,13 @@ const SignupForm = () => {
         <Button
           disabled={!(userFormData.username && userFormData.email && userFormData.password)}
           type='submit'
-          variant='success'>
+          variant='success'
+        >
           Submit
         </Button>
       </Form>
     </>
-  );
-};
+  )
+}
 
-export default SignupForm;
+export default SignupForm
